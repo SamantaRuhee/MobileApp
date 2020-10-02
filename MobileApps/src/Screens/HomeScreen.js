ea0e11d50af137abc98d190c6c2f96714155d4fe
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-10-02 20:57:27 +0600

    Homescreen created.

---
+++ MobileApps/src/Screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const HomeScreen = ()=>{
+    return(<Text>Hi there!</Text>);
+}
+
+const Styles = StyleSheet.create(
+    {
+        testStyle:{
+            fontSize: 30,
+            color: 'blue',
+        },
+    }
+);
+
+export default HomeScreen;
