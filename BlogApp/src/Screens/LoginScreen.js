b085dd955660ac0ae9c9fbad6709fbdafc825557
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-10-13 19:56:39 +0600

    app.js modified and empty login screen created.

---
+++ BlogApp/src/Screens/LoginScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, StyleSheet, Button, View, Image} from 'react-native';
+
+const LoginScreen = (props)=>{
+   
+};
+
+const Styles = StyleSheet.create(
+    {
+        textStyle:{
+            fontSize: 30,
+            color: 'blue',
+            alignContent: "center",
+            alignSelf: "center",
+        },
+    }
+);
+
+export default HomeScreen;
