d1fef751d46f69ed8d4d77434d0262336aa6635c
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-12-04 21:16:46 +0600

    signup signin screen done.

--- BlogAppAsync/App.js
+++ BlogAppAsync/App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import SignInScreen from './src/Screens/SignInScreen';
+import SignUpScreen from './src/Screens/SignUpScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const stack = createStackNavigator();
+
+function App(){
+  return(
+    <NavigationContainer initialRoutineName="SignInScreen">
+      <stack.Navigator>
+        <stack.Screen name="SignIn"
+        component={SignInScreen}
+        options={{ headerShown: false }}/>
+        <stack.Screen name="SignUp"
+        component={SignUpScreen}
+        options={{ headerShown: false }}/>
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ BlogAppAsync/src/Screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const HomeScreen = () => {
+    return(
+        <Text>Hello</Text>
+    );
+};
+
+export default HomeScreen;
