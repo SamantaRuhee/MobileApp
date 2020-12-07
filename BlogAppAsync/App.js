bc80c0ed66c5f3560a6343a629847f686693b9c3
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2020-12-07 19:38:06 +0600

    home screen done with async.

--- BlogAppAsync/App.js
+++ BlogAppAsync/App.js
@@ -1,23 +1,94 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import SignInScreen from './src/Screens/SignInScreen';
-import SignUpScreen from './src/Screens/SignUpScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import HomeScreen from "./src/Screens/HomeScreen";
+import NotificationScreen from "./src/Screens/NotificationScreen";
+import ProfileScreen from "./src/Screens/ProfileScreen";
+import SignUpScreen from "./src/Screens/SignUpScreen";
+import SignInScreen from "./src/Screens/SignInScreen";
 
-const stack = createStackNavigator();
+import { AuthContext, AuthProvider } from "./src/providers/AuthProvider";
+import { Entypo, AntDesign, Ionicons } from "@expo/vector-icons";
+const AuthStack = createStackNavigator();
+const PostStack = createStackNavigator();
+const HomeTab = createMaterialBottomTabNavigator();
+const AppDrawer = createDrawerNavigator();
 
-function App(){
-  return(
-    <NavigationContainer initialRoutineName="SignInScreen">
-      <stack.Navigator>
-        <stack.Screen name="SignIn"
+const AppDrawerScreen = () => {
+  return (
+    <AppDrawer.Navigator>
+      <AppDrawer.Screen name="Home" component={HomeTabScreen} />
+      <AppDrawer.Screen name="Profile" component={ProfileScreen} />
+    </AppDrawer.Navigator>
+  );
+};
+const HomeTabScreen = () => {
+  return (
+    <HomeTab.Navigator initialRouteName="Home">
+      <HomeTab.Screen
+        name="Home"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Entypo name="home" color="white" size={26} />
+            ) : (
+                <AntDesign name="home" color="white" size={22} />
+              ),
+        }}
+      />
+      <HomeTab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarLabel: "Notifications",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons name="ios-notifications" size={26} color="white" />
+            ) : (
+                <Ionicons
+                  name="ios-notifications-outline"
+                  size={22}
+                  color="white"
+                />
+              ),
+        }}
+      />
+    </HomeTab.Navigator>
+  );
+};
+
+const AuthStackScreen = () => {
+  return (
+    <AuthStack.Navigator initialRouteName="SignIn">
+      <AuthStack.Screen
+        name="SignIn"
         component={SignInScreen}
-        options={{ headerShown: false }}/>
-        <stack.Screen name="SignUp"
+        options={{ headerShown: false }}
+      />
+      <AuthStack.Screen
+        name="SignUp"
         component={SignUpScreen}
-        options={{ headerShown: false }}/>
-      </stack.Navigator>
-    </NavigationContainer>
+        options={{ headerShown: false }}
+      />
+    </AuthStack.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <AuthProvider>
+      <AuthContext.Consumer>
+        {(auth) => (
+          <NavigationContainer>
+            {auth.IsLoggedIn ? <AppDrawerScreen /> : <AuthStackScreen />}
+          </NavigationContainer>
+        )}
+      </AuthContext.Consumer>
+    </AuthProvider>
   );
 }
+
 export default App;
--- BlogAppAsync/src/Screens/HomeScreen.js
+++ BlogAppAsync/src/Screens/HomeScreen.js
@@ -1,10 +1,179 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, FlatList, ImageBackground } from "react-native";
+import { Card } from "react-native-elements";
+import { useNetInfo } from "@react-native-community/netinfo";
+import { getAllData, getDataJSON, storeDataJSON, removeData } from "../Functions/AsyncStorageFunctions";
+import { AuthContext } from "../Providers/AuthProvider";
 
+import PostCard from "./../Components/PostCard";
+import HeaderHome from "./../Components/HeaderHome";
 
-const HomeScreen = () => {
-    return(
-        <Text>Hello</Text>
+const getAllPosts = async () => {
+    let keys = await getAllData();
+    let allposts = [];
+    try {
+        if (keys != null) {
+            for (let key of keys) {
+                if (key.includes('post')) {
+                    let post = await getDataJSON(key);
+                    allposts.push(post);
+                }
+            }
+            return allposts;
+        }
+    } catch (error) {
+        alert(error);
+    }
+}
+const savePost = async (username, name, postID, input) => {
+    let currentPost = {
+        username: username,
+        name: name,
+        postID: postID,
+        post: input,
+        likes: 0,
+    };
+    storeDataJSON(
+        JSON.stringify(postID),
+        JSON.stringify(currentPost)
     );
+
+    alert("Post Saved!")
+    let UserData = await getDataJSON(JSON.stringify(postID));
+    console.log(UserData);
+}
+
+const getAllComments = async () => {
+    let keys = await getAllData();
+    let allComments = [];
+    try {
+        if (keys != null) {
+            for (let key of keys) {
+                if (key.includes('comment')) {
+                    let comment = await getDataJSON(key);
+                    allComments.push(comment);
+                }
+            }
+            return allComments;
+        }
+    } catch (error) {
+        alert(error);
+    }
+}
+
+const saveComment = async (postID, postAuthor, commentID, commneterID, commenterName, input) => {
+
+    let currentComment = {
+        post: postID,
+        reciever: postAuthor,
+        commentId: commentID,
+        commneterID: commneterID,
+        commenter: commenterName,
+        comment: input,
+    };
+
+    storeDataJSON(
+        JSON.stringify(commentID),
+        JSON.stringify(currentComment)
+    );
+
+    alert("Comment Saved!")
+    let UserData = await getDataJSON(JSON.stringify(commentID));
+    console.log(UserData);
+}
+
+const HomeScreen = (props) => {
+
+    const netinfo = useNetInfo();
+    if (netinfo.type != "unknown" && !netinfo.isInternetReachable) {
+        alert("No Internet!");
+    }
+
+    const [posts, setPosts] = useState([]);
+    const [postID, setpostID] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [input, setInput] = useState([]);
+
+
+    const loadPosts = async () => {
+        setLoading(true);
+        let response = await getAllPosts();
+        if (response != null) {
+            setPosts(response);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        loadPosts();
+    }, []);
+
+    if (!loading) {
+        return (
+            <AuthContext.Consumer>
+                {(auth) => (
+                    <View style={styles.viewStyle}>
+
+                        <HeaderHome
+                            DrawerFunction={() => {
+                                props.navigation.toggleDrawer();
+                            }}
+                        />
+                        <Card>
+                            <Input
+                                Text="What's On Your Mind?"
+                                leftIcon={<Entypo name="pencil" size={24} color="black" />}
+                                onChangeText={(currentText) => {
+                                    setInput(currentText);
+                                }}
+                                pressFunction={async () => {
+                                    setpostID([auth.CurrentUser.username + "-post-" + Math.random().toString(36).substring(7)]);
+                                    savePost(
+                                        auth.CurrentUser.username,
+                                        auth.CurrentUser.name,
+                                        postID,
+                                        input
+                                    )
+                                }}
+                            />
+                            <Button title="Post" type="outline" onPress={props.pressFunction} />
+                        </Card>
+                        <FlatList
+                            data={posts}
+                            onRefresh={loadPosts}
+                            refreshing={loading}
+                            renderItem={function ({ item }) {
+                                let data = JSON.parse(item)
+                                return (
+                                    <View>
+                                        <Card>
+                                            <PostCard
+                                                author={data.name}
+                                                body={data.post}
+                                                removeFunc={async () => {
+                                                    await removeData(JSON.stringify(data.postID))
+                                                    alert("Post Deleted!");
+                                                }}
+                                            />
+                                            <Card.Divider />
+                                        </Card>
+                                    </View>
+                                );
+                            }}
+                            keyExtractor={(item, index) => index.toString()}
+                        />
+                    </View>
+                )}
+            </AuthContext.Consumer>
+        );
+    } else {
+        return (
+            <View style={{ flex: 1, justifyContent: "center" }}>
+                <ActivityIndicator size="large" color="black" animating={true} />
+            </View>
+        );
+    }
 };
+
 
 export default HomeScreen;
